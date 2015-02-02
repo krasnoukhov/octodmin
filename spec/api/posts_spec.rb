4c2ee2657cd264efcfa22fb453072ff4aeabdfaf
Dmitry Krasnoukhov <dmitry@krasnoukhov.com>
2015-02-02 22:23:29 +0200

    Improve specs

--- spec/api/posts_spec.rb
+++ spec/api/posts_spec.rb
@@ -1,41 +1,50 @@
 require "spec_helper"
 
+shared_examples_for "existing post" do
+  it "has post" do
+    expect(subject["identifier"]).to eql("2015-01-30-test")
+    expect(subject["title"]).to eql("Test")
+    expect(subject["path"]).to eql("_posts/2015-01-30-test.markdown")
+    expect(subject["date"]).to eql("2015-01-30 18:00:47 +0200")
+    expect(subject["content"]).to eql("# OMG\n\nThis is a *post*!\n")
+    expect(subject["custom"]).to eql("data")
+  end
+end
+
+shared_examples_for "new post" do
+  it "has post" do
+    expect(File.exists?("sample/_posts/#{date}-new-one.markdown")).to be_truthy
+    expect(subject["identifier"]).to eql("#{date}-new-one")
+    expect(subject["title"]).to eql("New One")
+    expect(subject["path"]).to eql("_posts/#{date}-new-one.markdown")
+    expect(subject["date"]).to start_with(date)
+    expect(subject["content"]).to eql("New post from **template**\n")
+    expect(subject["custom"]).to eql("default")
+  end
+end
+
 describe "posts" do
   let(:app) { Octodmin::App.new }
+  let(:date) { Date.today.strftime("%Y-%m-%d") }
 
   describe "index" do
     before { get "/api/posts" }
     subject { parse_json(last_response.body)["posts"] }
 
-    it "returns posts" do
-      expect(subject.count).to eql(2)
-      expect(subject.first["identifier"]).to eql("2015-01-30-test")
-      expect(subject.first["title"]).to eql("Test")
-      expect(subject.first["excerpt"]).to eql("# OMG\n\n")
-      expect(subject.first["custom"]).to eql("data")
-
-      expect(subject.last["identifier"]).to eql("2015-01-30-welcome-to-jekyll")
-      expect(subject.last["title"]).to eql("Welcome to Jekyll!")
+    context "first post" do
+      subject { parse_json(last_response.body)["posts"].first }
+      it_behaves_like "existing post"
     end
-  end
 
-  describe "show" do
-    before { get "/api/posts/2015-01-30-test" }
-    subject { parse_json(last_response.body)["posts"] }
-
-    it "returns post" do
-      expect(subject["identifier"]).to eql("2015-01-30-test")
-      expect(subject["title"]).to eql("Test")
-      expect(subject["path"]).to eql("_posts/2015-01-30-test.markdown")
-      expect(subject["date"]).to eql("2015-01-30 18:00:47 +0200")
-      expect(subject["content"]).to eql("# OMG\n\nThis is a *post*!\n")
-      expect(subject["custom"]).to eql("data")
+    context "last post" do
+      specify do
+        expect(subject.last["identifier"]).to eql("2015-01-30-welcome-to-jekyll")
+        expect(subject.last["title"]).to eql("Welcome to Jekyll!")
+      end
     end
   end
 
   describe "create" do
-    let(:date) { Date.today.strftime("%Y-%m-%d") }
-
     context "invalid" do
       context "no title" do
         before { post "/api/posts" }
@@ -65,14 +74,20 @@ describe "posts" do
       after { File.delete("sample/_posts/#{date}-new-one.markdown") }
       subject { parse_json(last_response.body)["posts"] }
 
-      it "creates post" do
-        expect(subject["identifier"]).to eql("#{date}-new-one")
-        expect(subject["title"]).to eql("New One")
-        expect(subject["path"]).to eql("_posts/#{date}-new-one.markdown")
-        expect(subject["date"]).to start_with(date)
-        expect(subject["content"]).to eql("New post from **template**\n")
-        expect(subject["custom"]).to eql("default")
+      context "response" do
+        it_behaves_like "new post"
+      end
+
+      context "request" do
+        before { get "/api/posts/#{date}-new-one" }
+        it_behaves_like "new post"
       end
     end
+  end
+
+  describe "show" do
+    before { get "/api/posts/2015-01-30-test" }
+    subject { parse_json(last_response.body)["posts"] }
+    it_behaves_like "existing post"
   end
 end
